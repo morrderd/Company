d9c86502066bb2ced64bc057b970e9d015be34af
Mateusz Ćwiląg <mateuszcwilag@gmail.com>
2020-01-06 17:26:32 +0100

    added Client constructor and validation methods and unit test for validations

--- Company/Client.cs
+++ Company/Client.cs
@@ -8,10 +8,25 @@ namespace Company
 {
     public class Client
     {
+        public Client()
+        {
+
+        }
+
+        public Client(int clientId)
+        {
+            ClientId = clientId; 
+        }
+
+        public static int Counter { get; set; }
+
         public string Surname { get; set; }
+
         public string Name { get; set; }
+
         public string Email { get; set; }
-        public int ClientId { get; set; }
+
+        public int ClientId { get; private set; }
 
         public string FullName
         {
@@ -30,6 +45,32 @@ namespace Company
 
                 return FullName;
             }
+        }
+
+        public bool Validation(string property)
+        {
+            var correct = true;
+
+            if (string.IsNullOrWhiteSpace(property))
+            {
+                return !correct;
+            }
+            return correct;
+        }
+
+        public bool SaveClient()
+        {
+            return true;
+        }
+
+        public Client GetClient(int ClientId)
+        {
+            return new Client();
+        }
+
+        public List<Client> GetClient()
+        {
+            return new List<Client>();
         }
     }
 }
--- CompanyTests/ClientTest.cs
+++ CompanyTests/ClientTest.cs
@@ -10,44 +10,83 @@ namespace CompanyTests
         [TestMethod]
         public void FullNameTest()
         {
-            //Arange
             Client client = new Client();
             client.Name = "Imie";
             client.Surname = "Nazwisko";
 
             string expectedFullName = "Imie Nazwisko";
-            //Act
+            
             string actualFullName = client.FullName;
-            //Assert
+            
             Assert.AreEqual(expectedFullName, actualFullName);
         }
 
         [TestMethod]
         public void EmptyNameFullNameTest()
         {
-            //Arange
             Client client = new Client();
             client.Surname = "Nazwisko";
 
             string expectedFullName = "Nazwisko";
-            //Act
+
             string actualFullName = client.FullName;
-            //Assert
+
             Assert.AreEqual(expectedFullName, actualFullName);
         }
 
         [TestMethod]
         public void EmptySurrnameFullNameTest()
         {
-            //Arange
             Client client = new Client();
             client.Name = "Imie";
 
             string expectedFullName = "Imie";
-            //Act
+
             string actualFullName = client.FullName;
-            //Assert
+
             Assert.AreEqual(expectedFullName, actualFullName);
+        }
+
+        [TestMethod]
+        public void ClientCounterTest()
+        {
+            Client client1 = new Client();
+            client1.Name = "Klient1";
+            Client.Counter += 1;
+
+            Client client2 = new Client();
+            client2.Name = "Klient2";
+            Client.Counter += 1;
+
+            Client client3 = new Client();
+            client3.Name = "Klient3";
+            Client.Counter += 1;
+
+            Assert.AreEqual(3, Client.Counter);
+        }
+
+        [TestMethod]
+        public void PropertyValidation()
+        {
+            Client client = new Client();
+            client.Email = "mail@mail.com";
+
+            var expected = true;
+            var actual = client.Validation(client.Email);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PropertyValidationNull()
+        {
+            Client client = new Client();
+            client.Email = "";
+
+            var expected = false;
+            var actual = client.Validation(client.Email);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
