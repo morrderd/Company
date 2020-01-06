32e8aea96bd2aa5c3e4ba4f0ca53379178ee7eea
Mateusz Ćwiląg <mateuszcwilag@gmail.com>
2020-01-06 22:05:47 +0100

    added product and order

---
+++ Company/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    class Order
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(int orderId)
+        {
+            OrderId = orderId;
+        }
+
+        public int OrderId { get; private set; }
+        public DateTimeOffset? OrderDate { get; set; }
+    }
+}
---
+++ Company/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            ProductId = productId;
+        }
+
+        public int ProductId { get; private set; }
+        public Decimal? ActualPrice { get; set; }
+        public string Description { get; set; }
+        public string ProductName { get; set; }
+        
+    }
+}
